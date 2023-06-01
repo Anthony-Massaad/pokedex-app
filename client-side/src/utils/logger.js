ba4b6f9280bf303ef57ebb16ae4757c478050597
Anthony Massaad <massaadanthony@hotmail.com>
2023-06-01 09:47:01 -0400

    additional features into dev

---
+++ client-side/src/utils/logger.js
@@ -0,0 +1,21 @@
+import chalk from "chalk";
+
+const error = chalk.bold.red;
+const warning = chalk.bold.hex("#FFA500");
+const info = chalk.bold.green; 
+
+const log = (msg, type) => {
+    console.log(`${type}: `, msg);
+}
+
+export const logger = {
+    info: (msg) => {
+        log(msg, info("[Info]"))
+    },
+    warn: (msg) => {
+        log(msg, warning("[Warn]"))
+    },
+    error: (msg) => {
+        log(msg, error("[Error]"))
+    }
+};
