f54eff1e17df9b9a0871c243d4b2fbc50da30689
anthony-massaad <massaadanthony@hotmail.com>
2023-06-13 22:15:58 -0400

    revise

--- client-side/src/components/Modal.jsx
+++ client-side/src/components/Modal.jsx
@@ -86,8 +86,7 @@ const Modal = ({
       if (!isInvalidSignInInput()) {
         axios
           .get(
-            `http://127.0.0.1:8080/login?username=${usernameInput}&password=${passwordInput}`,
-            { credentials: "include" }
+            `http://127.0.0.1:8080/login?username=${usernameInput}&password=${passwordInput}`
           )
           .then((res) => {
             const res_data = res.data;
